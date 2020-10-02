754aa67b4e50c0751abf2bc62444af7e83573804
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-10-01 20:39:43 -0500

    solo me quedo con el metodo mas rapido.

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -1,13 +1,13 @@
 module Geometry
 
-export reshuffle, reshuffle_t
+export reshuffle
 
 function issquare(x::Int64)
     y = isqrt(x)
     return y*y == x
 end
 
-function reshuffle_t(input)
+function reshuffle(input)
     len,_ = size(input)
     n = len |> sqrt |> Integer
     lista = UnitRange{Int64}[]
@@ -23,23 +23,6 @@ function reshuffle_t(input)
         @inbounds copia[lista[i],lista[j]] = reshape(input[ind,:], n, n)|> transpose
         ind += 1
       end
-    end
-    copia
-end
-function reshuffle(input)
-    len,_ = size(input)
-    n = len |> sqrt |> Integer
-    lista = UnitRange{Int64}[]
-    copia = similar(input)
-
-    for i in 1:n
-        push!(lista,  (n*(i-1) + 1):n*i )
-    end
-
-    ind = 1
-    for i in lista, j in lista
-        copia[i,j] = reshape(input[ind,:], n, n)|> transpose
-        ind += 1
     end
     copia
 end
