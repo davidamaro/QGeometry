233573207d4290f0a82bcf4ab3f5b1568004013b
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-10-06 23:54:45 -0500

    Se añade generador de grupos HW.

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -1,6 +1,9 @@
 module Geometry
+import RandomMatrices: Haar
 
-export reshuffle
+export reshuffle, canalunistocastico,banano
+export puntosimplejo, soportematriz, canal
+export grupoHW
 
 function issquare(x::Int64)
     y = isqrt(x)
@@ -25,6 +28,74 @@ function reshuffle(input)
       end
     end
     copia
+end
+
+function canalunistocastico(mat)
+  len, _ = size(mat)
+  n = len |> sqrt |> Integer
+
+  reshuffle(mat)*conj(transpose(mat))
+end
+
+function banano()
+  mat1 = rand(Haar(2), 3)
+  mat2 = rand(Haar(2), 3)
+  mat3 = rand(Haar(2), 3)
+
+  mat = zeros(Complex{Float64}, 9, 9)
+  mat[1:3, 1:3] = mat1
+  mat[4:6, 4:6] = mat2
+  mat[7:9, 7:9] = mat3
+
+  mat
+end
+
+function canal(p₁,p₂)
+    s = (3*p₁-1) + im*(p₁+2*p₂-1)
+    [1 conj(s) s;
+     s 1 conj(s);
+     conj(s) s 1
+     ]
+end
+
+function soportematriz(mat)
+    abc = zeros(Complex{Float64}, 3,3)
+    for (i,x) in enumerate([1,5,9]), (j,y) in enumerate([1,5,9])
+        abc[i,j] = mat[x,y]
+    end
+    abc
+end
+
+function puntosimplejo(n)
+    x = rand(Float64,n - 1)
+
+    x = [0;x;1]
+
+    y = x[2:end] .- x[1:end-1]
+
+    z = rand()^(1/n)
+
+    return y.*z
+end
+
+function genQ(n)
+    ωₙ = ℯ^(2*π*im/n)
+    mat::Array{Complex{Float64}, 2} = zeros(Complex{Float64},n,n)
+    for i in 0:n-1
+        mat[i+1,i+1] = ωₙ^i
+    end
+    mat
+end
+function genP(n)
+    mat::Array{Complex{Float64}, 2} = zeros(Complex{Float64},n,n)
+    for i in 1:n-1
+        mat[i,i+1] = 1
+    end
+    mat[n,1] = 1
+    mat
+end
+function grupoHW(n)
+  genQ(n), genP(n)
 end
 
 end # module
